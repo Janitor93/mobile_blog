8f62ac366c15222dc29de3374efea3d227b56951
Ruslan Prnkovskij <ruslan.penkovskij@gmail.com>
2016-09-16 21:55:38 +0300

    ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var slideSpeed = 500;
-  var marginTop = 540;
+  var marginTop = 556;
   $(".comment-btn").on("click", function() {
     $(".main").animate({"marginTop": "-" + marginTop}, slideSpeed);
   }); //end on click
@@ -11,7 +11,17 @@ $(document).ready(function() {
     var marginRight;
     var circle;
     circle = $(this).index();
-    marginRight = 342 * circle;
-    $(".main").animate({"margin-right": "-" + marginRight}, slideSpeed);
+    marginRight = 312 * circle;
+    $(".circle").removeClass("dark");
+    $(this).addClass("dark");
+    $(".main").animate({"margin-left": "-" + marginRight}, slideSpeed);
   });
+  $(".travel-btn").on("click", function() {
+    $(".route").animate({"marginTop": 0}, slideSpeed);
+    $("footer").css("z-index", -1);
+  });
+  $(".choose-btn").on("click", function() {
+    $(".route").animate({"marginTop": -556}, slideSpeed);
+    $("footer").css("z-index", 5);
+  })
 }); //end ready
